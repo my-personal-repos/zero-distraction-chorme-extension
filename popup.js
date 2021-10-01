79531810f9439784834e56947ce03942093bd0fa
rayan-chakraborty <49035787+rayan-chakraborty@users.noreply.github.com>
2021-10-01 19:44:52 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,13 +1,13 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
     document.getElementById('url').innerText = tabs[0].title;
 })
-document.getElementById('btn').addEventListener('click', function(){
+document.querySelector('#btn').addEventListener('click', function(){
     chrome.tabs.query({active: true,currentWindow: true}, function(all_tab){
         window.open(all_tab[0].url, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=50,left=50,width=800,height=800");
         window.close()
     })
 })
-document.getElementById('temp-btn').addEventListener('click', function(){
+document.querySelector('#temp-btn').addEventListener('click', function(){
     chrome.tabs.query({active: true,currentWindow: true}, function(tabs){
         window.open(tabs[0].url, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=50,left=50,width=800,height=800");
         window.close()
